5160ba117bfc2932f105dfaf922880779fa32f60
AlexandraOricchio <aeoricchio@gmail.com>
2020-01-28 11:53:09 -0600

    Created working code for JS app 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -22,9 +22,7 @@ tableData.forEach(sighting => {
 
 function handleChange() {
     var newData = tableData;
-
     var elements = d3.selectAll(".filter");
-    // console.log(elements);
 
     inputDict = {};
 
@@ -33,8 +31,6 @@ function handleChange() {
         var filter = d3.select(this).select("input")
         var input= filter.property("value");
         var id = filter.property("id");
-        // console.log(id);
-        // console.log(input);
 
         if (input !== "") {
             console.log(`a filter was input for id: ${id}, with value: ${input}`);
@@ -42,55 +38,25 @@ function handleChange() {
             console.log(inputDict);
             Object.entries(inputDict).forEach(([key, value]) => {
                 console.log(key);
+                console.log(value);
                 newData = newData.filter(sighting => sighting[key] === value);
+                console.log(newData);
+
+                // clear data in table and fill table with filtered data
+                var tbody = d3.select("tbody");
+                tbody.html("");
+
+                // loop through filtered data and add to table
+                newData.forEach(sighting => {
+                    var row = tbody.append("tr");
+                    Object.entries(sighting).forEach(([key, value]) => {
+                        var cell = row.append("td");
+                        cell.text(value);
+                    });
+                });
             });
-            console.log(newData);
         };
-
-    // console.log(inputDict);
-
-    // Object.entries(inputDict).forEach(([key, value]) => {
-    //     console.log(key);
-    //     newData = newData.filter(sighting => sighting[key] == value);
-    //     console.log(newData);
-    // });
-
-    // console.log(newData);
-    
-        // newData = newData.filter(sighting => sighting[id] == filter);
-        
-        // console.log(newData);
-
     });
-
-    // for each input if value exists then newData = newData.filter
-
-    // // ** ORIGINAL CODE **
-    // // get input of filter element 
-    // var elementChg = d3.select(this).select("input")
-    // // get the value of that input element selected
-    // var elementVal = elementChg.property("value");
-    // console.log(elementVal);
-    // // get id for selected list item
-    // var elementID = elementChg.property("id");
-    // console.log(elementID);
-
-    // // filter table
-    // var filterData = tableData.filter(sighting => sighting[elementID] == elementVal);
-    // console.log(filterData);
-    
-    // // clear data in table and fill table with filtered data
-    // var tbody = d3.select("tbody");
-    // tbody.html("");
-    
-    // // loop through filtered data and add to table
-    // filterData.forEach(sighting => {
-    //     var row = tbody.append("tr");
-    //     Object.entries(sighting).forEach(([key, value]) => {
-    //         var cell = row.append("td");
-    //         cell.text(value);
-    //     });
-    // });
 };
 
 // create event listener for change
